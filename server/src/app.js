89a9232210e5d9ef74edf1d77f646ae5b70fe7aa
Riya Das <das.riya@gmail.com>
2017-09-17 15:31:54 +0530

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,27 @@
-console.log('hello');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get('/status',(req, res) => {
-	res.send({
+app.get('/status', (req, res) => {
+  res.send({
 
-		message: 'Hello World'
-	});
+    message: 'Hello World'
+  })
+})
 
-});
+app.post('/register', (req, res) => {
+  res.send({
 
-app.listen(process.env.PORT || 8081 );
+    message: `Hello ${req.body.email}Your user was registered! Have fun`
+  })
+})
+
+app.listen(process.env.PORT || 8081)
