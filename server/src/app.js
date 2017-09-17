3b5b02a37c3b77bb2fbab31cad652364ee1388c6
Riya Das <das.riya@gmail.com>
2017-09-17 22:44:32 +0530

    in between of db operation

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+require('./routes')(app)
 
 app.get('/status', (req, res) => {
   res.send({
@@ -17,11 +21,8 @@ app.get('/status', (req, res) => {
   })
 })
 
-app.post('/register', (req, res) => {
-  res.send({
-
-    message: `Hello ${req.body.email}Your user was registered! Have fun`
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
   })
-})
-
-app.listen(process.env.PORT || 8081)
---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+module.exports = (app) => {
+  app.post('/register', (req, res) => {
+    res.send({
+
+      message: `Hello ${req.body.email}Your user was registered! Have fun`
+    })
+  })
+}
